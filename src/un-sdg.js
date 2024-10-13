d92d1a9b4ef0ed55911785bf8489676a5089d3ff
Tylar Sanderson <tas6466@psu.edu>
2024-10-13 14:33:27 -0400

    svgs

--- src/un-sdg.js
+++ src/un-sdg.js
@@ -9,12 +9,13 @@ export class unSdg extends DDDSuper(LitElement) {
 
   constructor() {
     super();
-    this.title = "";
+    this.goal = "circle";
   }
 
   static get properties() {
     return {
-      title: { type: String },
+      goal: { type: String },
+      color: { type: String },
     };
   }
 
@@ -39,17 +40,82 @@ export class unSdg extends DDDSuper(LitElement) {
     `];
   }
 
+  updated(changedProperties) {
+    if (changedProperties.has('goal')) {
+      this.updateAlt();
+    }
+  }
+
+  updateAlt() { // combine color, image, and label
+    const goal = this.getAttribute('goal');
+    imgSrc = new URL(`../public/svgs/${this.goal}.svg`, import.meta.url).href;
+    
+    switch (goal) {
+      case 'circle':
+        this.label = "Sustainable developments logo"; 
+        break;
+      case 'all':
+        this.label = "Goal 1: No poverty, Goal 2: Zero hunger, Goal 3: Good health and well-being, Goal 4: Quality education, Goal 5: Gender equality, Goal 6: Clean water and sanitation, Goal 7: Affordable and clean energy, Goal 8: Decent work and economic growth, Goal 9: Industry, innovation and infrastructure, Goal 10: Reduced inequalities, Goal 11: Sustainable cities and communities, Goal 12: Responsible consumption and production, Goal 13: Climate action, Goal 14: Life below water, Goal 15: Life on land, Goal 16: Peace, justice and strong institutions, Goal 17: Partnerships for the goals";
+        break;
+      case '1':
+        this.label = "Goal 1: No poverty";
+        break;
+      case '2':
+        this.label = "Goal 2: Zero hunger";
+        break;
+      case '3':
+        this.label = "Goal 3: Good health and well-being";
+        break;
+      case '4':
+        this.label = "Goal 4: Quality education";
+        break;
+      case '5':
+        this.label = "Goal 5: Gender equality";
+        break;
+      case '6':
+        this.label = "Goal 6: Clean water and sanitation";
+        break;
+      case '7':
+        this.label = "Goal 7: Affordable and clean energy";
+        break;
+      case '8':
+        this.label = "Goal 8: Decent work and economic growth";
+        break;
+      case '9':
+        this.label = "Goal 9: Industry, innovation and infrastructure";
+        break;
+      case '10':
+        this.label = "Goal 10: Reduced inequalities";
+        break;
+      case '11':
+        this.label = "Goal 11: Sustainable cities and communities";
+        break;
+      case '12':
+        this.label = "Goal 12: Responsible consumption and production";
+        break;
+      case '13':
+        this.label = "Goal 13: Climate action";
+        break;
+      case '14':
+        this.label = "Goal 14: Life below water";
+        break;
+      case '15':
+        this.label = "Goal 15: Life on land";
+        break;
+      case '16':
+        this.label = "Goal 16: Peace, justice and strong institutions";
+        break;
+      case '17':
+        this.label = "Goal 17: Partnerships for the goals";
+        break; 
+  }
+
   render() {
-    return html`
-<div class="wrapper">
-  <div>${this.title}</div>
-  <slot></slot>
-</div>`;
+    return html;
   }
 
   static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url).href;
   }
 }
 
