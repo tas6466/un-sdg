1f94cfa451823afce02a9b07259afd2015e56339
Tylar Sanderson <tas6466@psu.edu>
2024-10-15 11:04:23 -0400

    idfk

--- src/un-sdg.js
+++ src/un-sdg.js
@@ -10,12 +10,26 @@ export class unSdg extends DDDSuper(LitElement) {
   constructor() {
     super();
     this.goal = "circle";
+    this.label = "";
+    this.width = "254px";
+    this.height = "254px";
+    this.loading = "lazy";
+    this.fetchPriority = "low";
+    this.colorOnly = false;
+    this.image = "";
   }
 
   static get properties() {
     return {
-      goal: { type: String },
+      goal: { type: String, reflect: true },
+      label: { type: String},
       color: { type: String },
+      width: { type: String },
+      height: { type: String },
+      loading: { type: String, reflect: true },
+      fetchPriority: { type: String, reflect: true},
+      colorOnly: { type: Boolean },
+      image: { type: String }
     };
   }
 
@@ -66,65 +80,101 @@ export class unSdg extends DDDSuper(LitElement) {
 
   updateElements() { // combine color, image, and label
     const goal = this.getAttribute('goal');
-    imgSrc = new URL(`../lib/svgs/${this.goal}.svg`, import.meta.url).href;
-    
     switch (goal) {
       case 'circle':
-        this.label = "Sustainable developments logo"; 
+        this.label = "Sustainable developments logo";
+        this.color = "white";
+        this.image = new URL(`../lib/svgs/circle.png`, import.meta.url).href;
         break;
       case 'all':
-        this.label = "Goal 1: No poverty, Goal 2: Zero hunger, Goal 3: Good health and well-being, Goal 4: Quality education, Goal 5: Gender equality, Goal 6: Clean water and sanitation, Goal 7: Affordable and clean energy, Goal 8: Decent work and economic growth, Goal 9: Industry, innovation and infrastructure, Goal 10: Reduced inequalities, Goal 11: Sustainable cities and communities, Goal 12: Responsible consumption and production, Goal 13: Climate action, Goal 14: Life below water, Goal 15: Life on land, Goal 16: Peace, justice and strong institutions, Goal 17: Partnerships for the goals";
+        this.label = "All Sustainable Development Goals";
+        this.color = "white";
+        this.image = new URL(`../lib/svgs/all.svg`, import.meta.url).href;
         break;
       case '1':
         this.label = "Goal 1: No poverty";
+        this.color = "var(--un-sdg-goal-1)";
+        this.image = new URL(`../lib/svgs/goal-1.svg`, import.meta.url).href;
         break;
       case '2':
         this.label = "Goal 2: Zero hunger";
+        this.color = "var(--un-sdg-goal-2)";
+        this.image = new URL(`../lib/svgs/goal-2.svg`, import.meta.url).href;
         break;
       case '3':
         this.label = "Goal 3: Good health and well-being";
+        this.color = "var(--un-sdg-goal-3)";
+        this.image = new URL(`../lib/svgs/goal-3.svg`, import.meta.url).href;
         break;
       case '4':
         this.label = "Goal 4: Quality education";
+        this.color = "var(--un-sdg-goal-4)";
+        this.image = new URL(`../lib/svgs/goal-4.svg`, import.meta.url).href;
         break;
       case '5':
         this.label = "Goal 5: Gender equality";
+        this.color = "var(--un-sdg-goal-5)";
+        this.image = new URL(`../lib/svgs/goal-5.svg`, import.meta.url).href;
         break;
       case '6':
         this.label = "Goal 6: Clean water and sanitation";
+        this.color = "var(--un-sdg-goal-6)";
+        this.image = new URL(`../lib/svgs/goal-6.svg`, import.meta.url).href;
         break;
       case '7':
         this.label = "Goal 7: Affordable and clean energy";
+        this.color = "var(--un-sdg-goal-7)";
+        this.image = new URL(`../lib/svgs/goal-7.svg`, import.meta.url).href;
         break;
       case '8':
         this.label = "Goal 8: Decent work and economic growth";
+        this.color = "var(--un-sdg-goal-8)";
+        this.image = new URL(`../lib/svgs/goal-8.svg`, import.meta.url).href;
         break;
       case '9':
         this.label = "Goal 9: Industry, innovation and infrastructure";
+        this.color = "var(--un-sdg-goal-9)";
+        this.image = new URL(`../lib/svgs/goal-9.svg`, import.meta.url).href;
         break;
       case '10':
         this.label = "Goal 10: Reduced inequalities";
+        this.color = "var(--un-sdg-goal-10)";
+        this.image = new URL(`../lib/svgs/goal-10.svg`, import.meta.url).href;
         break;
       case '11':
         this.label = "Goal 11: Sustainable cities and communities";
+        this.color = "var(--un-sdg-goal-11)";
+        this.image = new URL(`../lib/svgs/goal-11.svg`, import.meta.url).href;
         break;
       case '12':
         this.label = "Goal 12: Responsible consumption and production";
+        this.color = "var(--un-sdg-goal-12)";
+        this.image = new URL(`../lib/svgs/goal-12.svg`, import.meta.url).href;
         break;
       case '13':
         this.label = "Goal 13: Climate action";
+        this.color = "var(--un-sdg-goal-13)";
+        this.image = new URL(`../lib/svgs/goal-13.svg`, import.meta.url).href;
         break;
       case '14':
         this.label = "Goal 14: Life below water";
+        this.color = "var(--un-sdg-goal-14)";
+        this.image = new URL(`../lib/svgs/goal-14.svg`, import.meta.url).href;
         break;
       case '15':
         this.label = "Goal 15: Life on land";
+        this.color = "var(--un-sdg-goal-15)";
+        this.image = new URL(`../lib/svgs/goal-15.svg`, import.meta.url).href;
         break;
       case '16':
         this.label = "Goal 16: Peace, justice and strong institutions";
+        this.color = "var(--un-sdg-goal-16)";
+        this.image = new URL(`../lib/svgs/goal-16.svg`, import.meta.url).href;
         break;
       case '17':
         this.label = "Goal 17: Partnerships for the goals";
+        this.color = "var(--un-sdg-goal-17)";
+        this.image = new URL(`../lib/svgs/goal-17.svg`, import.meta.url).href;
         break;
     }
   }  
